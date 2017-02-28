e55419c3d9bd1ec1266d466cc59d07539fd2ee62
nay3 <y.ohba@everyleaf.com>
2017-02-28 14:24:31 +0900

    Initial commit

---
+++ lib/punctual_date_select.rb
@@ -0,0 +1,7 @@
+require "punctual_date_select/version"
+
+module PunctualDateSelect
+end
+
+require "punctual_date_select/model"
+require "punctual_date_select/helper"
---
+++ lib/punctual_date_select/helper.rb
@@ -0,0 +1,22 @@
+module PunctualDateSelect::Helper
+
+  module Builder
+    def punctual_date_select(method, options={}, html_options={})
+      @template.select_date(object.send(method) , {:prefix => "#{@object_name}[#{method}]"}.merge(options), html_options)
+    end
+  end
+
+  module DateHelper
+    def select_year
+      old_datetime = @datetime
+      @datetime = nil if @datetime.respond_to?(:year) && !@datetime.year
+      ret = super
+      @datetime = old_datetime
+      ret
+    end
+  end
+
+end
+
+ActionView::Helpers::FormBuilder.send(:include, PunctualDateSelect::Helper::Builder)
+ActionView::Helpers::DateHelper.send(:prepend, PunctualDateSelect::Helper::DateHelper)
